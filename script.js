2c55f475d394f0d6cd5de0fc41b1b1db75e58dba
Mohammad Abu Mualla <m2872001@gmail.com>
2023-12-17 22:36:02 +0200

    add the task

---
+++ script.js
@@ -0,0 +1,6 @@
+// part one
+document.querySelector("div").textContent = "demo class will applied here";
+document.querySelector("div").classList.add("demo");
+// part two
+document.querySelector("h1").textContent = "vip class will applied here";
+document.querySelector("h1").classList.remove("vip");
